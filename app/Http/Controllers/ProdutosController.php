b4fa35e8b4231e1de86af0d5ef328816fab9ba0b
igor parro <igorparroarcas@gmail.com>
2023-12-19 16:18:14 -0300

    filter

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -7,9 +7,16 @@ use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
-    public function index()
+    private $produto;
+    public function __construct(Produto $produto)
     {
-        $findProduto = Produto::all();
+        $this->produto = $produto;
+    }
+
+    public function index(Request $request)
+    {
+        $pesquisar = $request->pesquisar;
+        $findProduto = $this->produto->getProdutoPesquisarIndex(search: $pesquisar ?? '');
 
         return view('pages.produtos.paginacao', compact('findProduto'));
     }
